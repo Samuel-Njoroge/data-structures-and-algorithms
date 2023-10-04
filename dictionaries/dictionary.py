76ce6235fd196a1b64868831ac966a65966e0edf
N S <njorogekiragu8@gmail.com>
2023-10-04 05:37:59 +0000

    dictionaries

---
+++ dictionaries/dictionary.py
@@ -0,0 +1,18 @@
+"""
+1. Defined using key and value pairs
+2. Are unordered
+3. Keys are unique
+4. Keys are immutable
+"""
+
+# Creating a dictionary
+employee_ranks = {1 : 'CEO', 2 : 'Director', 3 : 'Manager'}
+print(employee_ranks)
+
+# Creating an empty dictionary
+empty_dict = {}
+print('An empty dictionary', empty_dict)
+
+# Creating a sdictionary using dict() method
+employee_ranks = dict({1 : 'CEO', 2 : 'Director', 3 : 'Manager'})
+print(employee_ranks)
