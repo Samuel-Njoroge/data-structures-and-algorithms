5a20b7be358b251dceca7870a8912b9ac9b984b3
Samuel-Njoroge <njorogekiragu8@gmail.com>
2023-10-10 09:25:12 +0300

    sets

---
+++ sets/sets.py
@@ -0,0 +1,72 @@
+"""
+1. Sets contain unique items, do not allow duplicates.
+2. Sets are unordered.
+3. Sets are mutable.
+4. Sets are unindexed.
+5. Sets can contain any data type.
+6. Sets can be empty.
+7. Sets cannot be nested.
+"""
+
+# Generating a set of cities in Kenya
+cities = ('Nairobi', 'Mombasa', 'Kisumu', 'Nakuru', 'Eldoret')
+print(cities)
+
+# Generating a set of even numbers from 0 to 20
+even_numbers = {0, 2, 4, 6, 8, 10, 12, 14, 16, 18}
+print(even_numbers)
+
+# Empty set
+empty_set = set()
+print(empty_set)
+
+# Length of a set
+print(len(cities))
+
+# type()
+odd_numbers = {1, 3, 5}
+print(type(odd_numbers))
+
+# Accessing items in a set
+odd_numbers = {1, 3, 5, 7, 9}
+
+for number in odd_numbers:
+    print(number)
+
+# Checking presence of an item in a set
+colors = {'blue', 'white', 'black', 'yellow', 'red'}
+print('blue' in colors)
+
+# Changing items in a set - You cannot change items in a set
+
+# Adding items to a set
+colors = {'blue', 'white', 'black', 'yellow', 'red'}
+colors.add('magenta')
+print(colors)
+
+# Adding multiple items to a set from a set
+east_africa = {'Kenya', 'Uganda', 'Tanzania'}
+west_africa = {'Nigeria', 'Ghana', 'Benin'}
+countries = east_africa.union(west_africa)
+print(countries)
+
+# Adding multiple items to a set from a set (2)
+east_africa = {'Kenya', 'Uganda', 'Tanzania'}
+east_africa.update(['Rwanda', 'Burundi'])
+print(east_africa)
+
+# Adding multiple items to a set from a list
+west_africa = {'Nigeria', 'Ghana', 'Benin'}
+others = ['Liberia', 'Sierra Leone']
+west_africa.update(others)
+print(west_africa)
+
+# Removing items from a set - Method 1
+colors = {'blue', 'white', 'black', 'yellow', 'red'}
+colors.remove('red')
+print(colors)
+
+# Removing items from a set - Method 2
+colors = {'blue', 'white', 'black', 'yellow', 'red'}
+colors.discard('blue')
+print(colors)
