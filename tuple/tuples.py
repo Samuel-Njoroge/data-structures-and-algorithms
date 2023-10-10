ee59ea3d5c2beb5129cfeef9b5e5b7a86b1756a7
Samuel-Njoroge <njorogekiragu8@gmail.com>
2023-10-10 10:05:31 +0300

    tuples

--- tuple/tuples.py
+++ tuple/tuples.py
@@ -20,3 +20,48 @@ print(even_numbers)
 # Generating a tuple of a mix of data types
 mix_tuples = (4, 8, 20, 'two', 'four', 'twenty')
 print(mix_tuples)
+
+# Generating an empty tuple
+empty_tuple = ()
+print(empty_tuple)
+
+# One item tuple
+one_item_tuple = ('mango',)
+print(one_item_tuple)
+
+# type() function
+print(type(one_item_tuple))
+
+# Length of a tuple
+odd_numbers = (1, 3, 5, 7, 9, 11)
+print(len(odd_numbers))
+
+# Accessing items in a tuple
+odd_numbers = (1, 3, 5, 7, 9, 11)
+print(odd_numbers[1])
+
+# Accessing items in a tuple - Negative indexing
+odd_numbers = (1, 3, 5, 7, 9, 11)
+print(odd_numbers[-1])
+
+# Accessing items in a tuple - Range indexing
+odd_numbers = (1, 3, 5, 7, 9, 11)
+print(odd_numbers[1:5])
+
+# Changing tuple values - Tuples are immutable
+
+# Adding items to a tuple - Tuples are immutable
+# Convert the tuple into a list, add items, then convert back to a tuple
+odd_numbers = (1, 3, 5, 7, 9, 11)
+odd_numbers_list = list(odd_numbers)
+odd_numbers_list.append(13)
+odd_numbers = tuple(odd_numbers_list)
+print(odd_numbers)
+
+# Removing items from a tuple - Tuples are immutable
+# Convert the tuple into a list, remove items, then convert back to a tuple
+odd_numbers = (1, 3, 5, 7, 9, 11)
+odd_numbers_list = list(odd_numbers)
+odd_numbers_list.remove(11)
+odd_numbers = tuple(odd_numbers_list)
+print(odd_numbers)
