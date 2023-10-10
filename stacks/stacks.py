19d08d743ea3e9f6f35cd8d3e9c26273cab03b2b
Samuel-Njoroge <njorogekiragu8@gmail.com>
2023-10-10 10:26:20 +0300

    stacks

---
+++ stacks/stacks.py
@@ -0,0 +1,11 @@
+"""
+User defined data structure that follow the LIFO (Last In First Out) or First In Last Out (FILO) principle.
+Examples of stacks in real life include:
+1. A stack of books
+2. A stack of plates
+3. A stack of pancakes
+4. A stack of coins
+5. A stack of chairs
+"""
+
+# Creating a stack
