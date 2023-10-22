e3bb54cd1df7227302d706db0e699557472e460d
Samuel-Njoroge <njorogekiragu8@gmail.com>
2023-10-22 09:51:13 +0300

    insertion in a singly linked list

--- linked_lists/singly_linked_list.py
+++ linked_lists/singly_linked_list.py
@@ -50,7 +50,7 @@ In between
 """
 
 class Node:
-    def __init__(self, data) -> None:
+    def __init__(self, data):
         self.data = data
         self.ref = None
 
@@ -58,13 +58,13 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-    def print_LL(self):
+    def print_LinkedList(self):
         if self.head is None:
-            print("Linked list is empty !")
+            print("Linked list is empty!")
         else:
             n = self.head
             while n is not None:
                 print(n.data)
                 n = n.ref
 LL1 = LinkedList()
-LL1.print_LL()
+LL1.print_LinkedList()
