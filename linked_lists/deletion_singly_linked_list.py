ac87b71ceda6841bdf1611d8935617585c79d98e
Samuel-Njoroge <njorogekiragu8@gmail.com>
2023-11-01 22:26:06 +0300

    linked lists

--- linked_lists/deletion_singly_linked_list.py
+++ linked_lists/deletion_singly_linked_list.py
@@ -1,4 +1,4 @@
-# Deletion at the beginning of the node
+# Deletion of a node
 
 # Insertion of elements in the node
 class Node:
@@ -80,13 +80,23 @@ class LinkedList:
             self.head = new_node
         else:
             print("Linked list is not empty!")
-
+    
+    # Deletion at the beginning
     def delete_begin(self):
         if self.head is None:
             print("Linked List is empty, can't delete nodes!")
         else:
             self.head = self.head.ref
 
+    # Deletion at the end
+    def delete_end(self):
+        if self.head is None:
+            print("Linked list is enpty, can't delete node")
+        else:
+            n = self.head
+            while n.ref.ref is not None:
+                n = n.ref
+            n.ref = None
 
 LL1 = LinkedList()
 LL1.add_begin(20)
@@ -101,4 +111,7 @@ LL1.add_before(25, 30)
 LL1.add_before(95, 100)
 
 LL1.delete_begin()
+LL1.print_LinkedList()
+
+LL1.delete_end()
 LL1.print_LinkedList()
