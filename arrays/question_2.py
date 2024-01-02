05412ec16a57649530d45c5ab6f021af3a072509
Samuel-Njoroge <njorogekiragu8@gmail.com>
2024-01-02 21:11:47 +0300

    arrays

--- arrays/question_2.py
+++ arrays/question_2.py
@@ -4,3 +4,15 @@ An array is monotone increasing if all its elements from left to right are non-d
 An array is monotone decreasing if all  its elements from left to right are non-increasing. 
 Given an integer array return true if the given array is monotonic, or false otherwise.
 """
+
+def monotonic_array(input_array):
+
+    if input_array[0] < input_array[1]:
+        print(True)
+    else:
+        print(False)
+
+monotonic_array([1, 7, 9, 12])
+
+
+    
