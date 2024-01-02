5fc95f87808e14da031c91a8735e933b42b6c8ec
Samuel-Njoroge <njorogekiragu8@gmail.com>
2024-01-02 20:56:51 +0300

    arrays

--- arrays/question_1.py
+++ arrays/question_1.py
@@ -3,5 +3,15 @@ Sorted Squared Array - You are given an array of Integers in which each subseque
 Write a function that takes this array as an input and returns a new array with the squares of each number sorted in ascending order.
 """
 
-def sorted_squared_array():
-    sorted_squared_array()
+def sorted_squared_array(my_array):
+    squared_array = []
+
+    for number in my_array:
+        squared_array.append(number **2)
+
+    # Sort the squared array   
+    squared_array.sort()
+
+    return squared_array
+
+sorted_squared_array([2,3,7,1])
